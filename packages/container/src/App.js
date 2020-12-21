56ccaf24a21c13fc18f3dd9fd5bf85d5a4cfb537
berlink2 <berlink.k2@gmail.com>
2020-12-21 20:01:45 +0700

    update

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import MarketingApp from './Components/MarketingApp';
-//
-//
+import Header from './Components/Header';
+import { BrowserRouter } from 'react-router-dom';
+
 export default () => {
-  return <MarketingApp />;
+  return <>
+  <Header />
+  <MarketingApp />
+  </>;
 };
